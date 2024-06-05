1b91811b23c418e9fcdcbbdbccf7e06a5538f2e3
Alphax978 <jamesrawal9191@gmail.com>
2024-06-05 18:57:25 +0545

    modal for get your quota web development

---
+++ app/Components/Webdevelopmentmodalquota/WBModal.jsx
@@ -0,0 +1,129 @@
+"use client";
+
+import React, { useState } from 'react';
+import { Modal, Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const WBModal = ({ openModalOrder, closeModalOrder, packagetypes }) => {
+
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [address, setAddress] = useState('')
+
+    function modalClose() {
+        closeModalOrder(false)
+        setFirstName('')
+        setLastName('')
+        setAddress('')
+        setEmail('')
+
+    }
+
+   
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        toast("Form submitted successfully!");
+        setFirstName('')
+        setLastName('')
+        setAddress('')
+        setEmail('')
+    }
+
+
+    return (
+        <div>
+            <Modal show={openModalOrder} onClose={modalClose} >
+                <Modal.Header className='text-center'>
+                    Fill Out the Form
+                </Modal.Header>
+                
+                <Modal.Body >
+                    <form onSubmit={handleSubmit}>
+                        <div className="space-y-6">
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="firstname" value="First Name" />
+                                </div>
+                                <TextInput
+                                    id="firstname"
+                                    value={firstname}
+                                    onChange={(event) => setFirstName(event.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="lastname" value="Last Name" />
+                                </div>
+                                <TextInput
+                                    id="lastname"
+                                    value={lastname}
+                                    onChange={(event) => setLastName(event.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email" value="Email" />
+                                </div>
+                                <TextInput
+                                    id="email"
+                                    value={email}
+                                    onChange={(event) => setEmail(event.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="address" value="Address" />
+                                </div>
+                                <TextInput
+                                    id="address"
+                                    value={address}
+                                    onChange={(event) => setAddress(event.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="package" value="Package" />
+                                </div>
+                                <TextInput
+                                    id="package"
+                                    defaultValue={packagetypes}
+                                    readOnly
+                                    required
+                                />
+                            </div>
+
+                            <div className="w-full">
+                                <Button type='submit'>Submit</Button>
+                            </div>
+                        </div>
+
+                    </form>
+                    <div className='mt-6'>
+                        <ToastContainer
+                            position="top-center"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            theme="light"
+                        />   
+                    </div>
+                </Modal.Body>
+
+            </Modal>
+        </div>
+    );
+};
+
+export default WBModal;
